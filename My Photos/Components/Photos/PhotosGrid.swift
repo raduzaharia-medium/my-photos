c60aac8e4dd29021c9b9a07a8b2bf1d9d943f20d
Radu Zaharia <raduzaharia@outlook.com>
2025-10-17 19:13:20 +0300

    Preparing the photos filter.

--- My Photos/Components/Photos/PhotosGrid.swift
+++ My Photos/Components/Photos/PhotosGrid.swift
@@ -12,7 +12,7 @@ struct PhotosGrid: View {
     init() {
         self._photos = Query(filters: presentationState.photoFilter)
     }
-
+    
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -62,94 +62,9 @@ struct PhotosGrid: View {
 
 extension Query where Element == Photo, Result == [Photo] {
     fileprivate init(filters: Set<SidebarItem>) {
-        // Build a compound predicate by intersecting all selected sidebar filters
-        let selectedDates = filters.selectedDates
-        var yearKeys: [Int] = []
-        var monthKeys: [(year: Int, month: Int)] = []
-        var dayKeys: [(year: Int, month: Int, day: Int)] = []
+        let filter = #Predicate<Photo> { $0.isDeleted == false }
+        let sort = [SortDescriptor(\Photo.dateTaken)]
 
-        for d in selectedDates {
-            switch d {
-            case .year(let y): yearKeys.append(y.year)
-            case .month(let m):
-                monthKeys.append((year: m.year.year, month: m.month))
-            case .day(let day):
-                dayKeys.append(
-                    (
-                        year: day.month.year.year,
-                        month: day.month.month,
-                        day: day.day
-                    )
-                )
-            }
-        }
-
-        //        let selectedTags = filters.selectedTags
-        //        let selectedAlbums = filters.selectedAlbums
-        //        let selectedEvents = filters.selectedEvents
-        //        let selectedPeople = filters.selectedPeople
-        //        let selectedPlaces = filters.selectedPlaces
-
-        let filter = #Predicate<Photo> { $0.isDeleted == false
-            // yearKeys.isEmpty && monthKeys.isEmpty && dayKeys.isEmpty
-
-            //                || (!yearKeys.isEmpty
-            //                    && yearKeys.contains { y in photo.dateTakenYear?.year == y })
-            //                || (!monthKeys.isEmpty
-            //                    && monthKeys.contains { key in
-            //                        (photo.dateTakenYear?.year == key.year)
-            //                            && (photo.dateTakenMonth?.month == key.month)
-            //                    })
-            //                || (!dayKeys.isEmpty
-            //                    && dayKeys.contains { key in
-            //                        (photo.dateTakenYear?.year == key.year)
-            //                            && (photo.dateTakenMonth?.month == key.month)
-            //                            && (photo.dateTakenDay?.day == key.day)
-            //                    })
-
-            //            // Tags: pass if none selected; else require all selected exist in photo.tags
-            //            let matchesTags =
-            //                selectedTags.isEmpty
-            //                || selectedTags.allSatisfy { t in
-            //                    photo.tags.contains { $0.id == t.id }
-            //                }
-            //
-            //            // Albums: pass if none selected; else require all selected albums match
-            //            let matchesAlbums =
-            //                selectedAlbums.isEmpty
-            //                || selectedAlbums.allSatisfy { a in
-            //                    photo.album == a
-            //                }
-            //
-            //            // People: pass if none selected; else require all present
-            //            let matchesPeople =
-            //                selectedPeople.isEmpty
-            //                || selectedPeople.allSatisfy { p in
-            //                    (photo.people?.contains { $0.id == p.id } ?? false)
-            //                }
-            //
-            //            // Events: pass if none selected; else require all match
-            //            let matchesEvents =
-            //                selectedEvents.isEmpty
-            //                || selectedEvents.allSatisfy { e in
-            //                    photo.event == e
-            //                }
-            //
-            //            // Places: pass if none selected; else match any of selected places
-            //            let matchesPlaces =
-            //                selectedPlaces.isEmpty
-            //                || selectedPlaces.contains { place in
-            //                    switch place {
-            //                    case .country(let c): photo.country == c
-            //                    case .locality(let l): photo.locality == l
-            //                    }
-            //                }
-            //
-            //            return matchesDate && matchesTags && matchesAlbums && matchesPeople
-            //                && matchesEvents && matchesPlaces
-        }
-
-        let sort = [SortDescriptor(\Photo.dateTaken, order: .reverse)]
         self.init(filter: filter, sort: sort)
     }
 }
